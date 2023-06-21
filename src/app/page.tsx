f98a3ee14f8bd9e1d7e306a2f77de5894685ab60
Alex B <mabrave1106@gmail.com>
2023-06-21 15:23:02 -0400

    chore: cleaned up the code a bit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import Image from 'next/image'
-import cardStyles from './styles/css/card.module.css';
+import Image from "next/image"
+import css from "./styles/css/card.module.css";
+import {styles} from "./styles/";
 import CardDescription from "@/components/cards/CardDescription";
 import CardFooter from "@/components/cards/CardFooter";
 
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div id="wrapper" className={cardStyles.wrapper}>
-                <section className={cardStyles.main_card}>
-          <span className={cardStyles.avatar}>
-            <Image src="/Screenshot-from-2023-02-21-12-16-29.svg"
-                   alt="A black and white picture of Alex"
-                   width={500}
-                   height={500}
-            />
+        <main className={styles.main}>
+            <div id="wrapper" className={css.wrapper}>
+                <section className={css.main_card}>
+                    <span className={css.avatar}>
+                        <Image src="/Screenshot-from-2023-02-21-12-16-29.svg"
+                               alt="A black and white picture of Alex"
+                               width={500}
+                               height={500}
+                        />
                     </span>
                     <CardDescription/>
                     <CardFooter/>
---
+++ src/app/styles/index.ts
@@ -0,0 +1,6 @@
+export const styles = {
+    main: "flex min-h-screen flex-col items-center justify-between p-24",
+    cardWrapper: "wrapper flex items-center justify-between flex-col perspective-1000 relative min-h-screen p-6 z-2 border-solid",
+    wrapperBefore: "block",
+}
+
