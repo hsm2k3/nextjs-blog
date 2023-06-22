2c94578507e7ec55ed46443e148d343639b5da15
Alex B <mabrave1106@gmail.com>
2023-06-22 15:27:48 -0400

    feat:  added some links in navbar.

--- src/components/bars/Navbar.tsx
+++ src/components/bars/Navbar.tsx
@@ -3,6 +3,8 @@
 import { motion } from 'framer-motion';
 import { navVariants} from "@/utils/motion";
 import { styles } from "@/app/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
     return (
@@ -12,7 +14,12 @@ export const Navbar = () => {
             initial="hidden"
             whileInView="show"
             >
-            nav
+            <div className="text-lg font-bold">Navbar</div>
+            <div className="flex space-x-4">
+                <a href="#" className="text-blue-500 hover:text-blue-700"><FontAwesomeIcon icon={faUser} /> {" "} About Me</a>
+                <a href="#" className="text-blue-500 hover:text-blue-700">Projects</a>
+                <a href="#" className="text-blue-500 hover:text-blue-700">Blog</a>
+            </div>
         </motion.nav>
     )
 }
