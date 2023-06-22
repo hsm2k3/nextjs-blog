07cf9585d7077fef29d8a3ef34d02a604c306167
Alex B <mabrave1106@gmail.com>
2023-06-22 15:30:16 -0400

    feat:  added coming soon text for prolog. updated resume.

--- src/app/prologue/page.tsx
+++ src/app/prologue/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div>
-            <h1>Hi</h1>
+        <div className={"flex min-h-screen flex-col items-center justify-between p-24"}>
+            <h1>Coming soon...</h1>
         </div>
     );
 };
