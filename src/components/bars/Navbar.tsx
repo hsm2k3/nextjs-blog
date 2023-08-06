f4e31010a0f2065df9c788bda6bb0a2e7651eb12
Alex B <mabrave1106@gmail.com>
2023-08-06 13:59:30 -0400

    feat:  redesigned the navbar to have a mobile first look.

--- src/components/bars/Navbar.tsx
+++ src/components/bars/Navbar.tsx
@@ -5,9 +5,16 @@ import { navVariants} from "@/utils/motion";
 import { styles } from "@/app/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faDiagramProject, faBlog } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import {useState} from "react";
 
 export const Navbar = () => {
 
+    const [isNavbarVisible, setIsNavbarVisible] = useState(false); // State variable to control visibility
+
+    const toggleNavbar = () => {
+        setIsNavbarVisible(!isNavbarVisible);
+    };
 
     return (
         <motion.nav
@@ -17,7 +24,17 @@ export const Navbar = () => {
             whileInView="show"
             >
             <div className="flex space-x-4">
-                <ul className="space-y-4">
+                {/* Hamburger Icon */}
+                <div className="cursor-pointer" onClick={toggleNavbar}>
+                    <Image
+                        src={"/menu.svg"}
+                        alt="Menu"
+                        width={50}
+                        height={50}
+                    />
+                </div>
+                {/* Navigation Links */}
+                <ul className={`space-y-4 ${isNavbarVisible ? 'block' : 'hidden'}`}> {/* Use CSS classes to show/hide */}
                     <li>
                         <a href="#">
                             <FontAwesomeIcon icon={faUser} /> {" "} About Me
