fe48be62b0d1f3714b48ba5005e58e040c130bd7
Alex B <mabrave1106@gmail.com>
2023-08-25 15:14:46 -0400

    feat:  added some middleware.

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import {NextRequest, NextResponse, userAgent} from "next/server";
+
+export const config = {
+    matcher: ['/prologue'],
+}
+
+export function middleware(request: NextRequest) {
+    const url = request.nextUrl.clone();
+    const { device } = userAgent(request);
+    const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
+    url.searchParams.set('viewport', viewport);
+    return NextResponse.rewrite(url);
+}
