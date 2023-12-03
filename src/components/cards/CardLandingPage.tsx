d2d96f97a893860a9c02655eb784ada986771e57
Alex B <mabrave1106@gmail.com>
2023-12-02 21:13:41 -0500

    refactor: svg path changed

--- src/components/cards/CardLandingPage.tsx
+++ src/components/cards/CardLandingPage.tsx
@@ -26,7 +26,7 @@ export const CardLandingPage = () => {
         <div id="wrapper" className={css.wrapper}>
             <section className={css.main_card}>
                     <span className={css.avatar}>
-                        <Image src="/Screenshot-from-2023-02-21-12-16-29.svg"
+                        <Image src="/alex.svg"
                                alt="A black and white picture of Alex"
                                width={500}
                                height={500}
