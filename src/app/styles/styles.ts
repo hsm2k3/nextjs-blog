e15decae300d590419a426cd11a2f58c04b635a8
Alex B <mabrave1106@gmail.com>
2024-03-08 02:17:49 -0500

    chore: moved colors into variables for styles component.

--- src/app/styles/styles.ts
+++ src/app/styles/styles.ts
@@ -1,10 +1,18 @@
 export const styles = {
-    main: "flex min-h-screen flex-col items-center justify-between p-24",
+    main: "flex min-h-screen flex-col items-center justify-between ${colors.background.gray} p-24",
     cardWrapper: "wrapper flex items-center justify-between flex-col perspective-1000 relative min-h-screen p-6 z-2 border-solid",
     wrapperBefore: "block",
-    navbar: "fixed top-0 left-0 flex items-start p-4 bg-gray-200 h-screen",
-    prologue: "flex min-h-screen flex-col items-center justify-start p-24",
-    starWarsIntro: "flex justify-center items-center h-screen bg-black text-yellow-400 overflow-hidden",
+    navbar: "fixed top-0 left-0 flex items-start p-4 ${colors.background.gray} h-screen",
+    prologue: "flex min-h-screen flex-col items-center ${colors.background.gray} justify-start p-24",
+    starWarsIntro: "flex justify-center items-center h-screen ${colors.background.gray} ${colors.text.black} overflow-hidden",
     starWarsIntroTitle: "text-3xl text-center mb-16"
 }
 
+const colors = {
+  background: {
+    gray: 'bg-gray-200'
+  },
+  text: {
+    black: 'text-black-400'
+  }
+}
