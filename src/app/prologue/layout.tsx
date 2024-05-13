99f57baeee200ea1bd3dbcc7786a79ce829a2cb0
Alex B <mabrave1106@gmail.com>
2024-05-13 09:16:48 -0400

    chore:  added notes

--- src/app/prologue/layout.tsx
+++ src/app/prologue/layout.tsx
@@ -2,6 +2,11 @@ import {Navbar} from "@/components/bars/Navbar";
 import React from "react";
 import {styles} from "@/app/styles/styles";
 
+// this function is responsible for rendering the layout of the prologue page.
+// it takes a single argument, children, which is a ReactNode.
+// the children are the components that are passed to the PrologueLayout.
+// the PrologueLayout is a wrapper for the Navbar and the children.
+// the Navbar is a component that is imported from the @/componenets/bars directory.
 export default function PrologueLayout({ children }: { children: React.ReactNode }) {
   return (
       <div className={styles.prologue}>
