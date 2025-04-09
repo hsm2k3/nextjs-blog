01d399aec7d88171747bd5eb2bd12fec519949a9
Alex B <mabrave1106@gmail.com>
2025-04-09 18:23:53 -0400

    fix: attempting to upgrade - related issue #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import Image from 'next/image'
+
 import cardStyles from '@/app/styles/css/card.module.css';
-import CardDescription from "@/components/cards/CardDescription";
-import CardFooter from "@/components/cards/CardFooter";
+
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div id="wrapper" className={cardStyles.wrapper}>
                 <section className={cardStyles.main_card}>
-          <span className={cardStyles.avatar}>
-            <Image src="/alex.svg"
-                   alt="A black and white picture of Alex"
-                   width={500}
-                   height={500}
-            />
-                    </span>
-                    <CardDescription/>
-                    <CardFooter/>
+
                 </section>
             </div>
         </main>
