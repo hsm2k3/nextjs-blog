a68bcb4a98832c6313453568a19d0069b107631b
Alex B <mabrave1106@gmail.com>
2025-05-08 19:54:29 -0400

    fix: troubleshooting - commented DynamicFontAwesomeIcon

--- src/components/fontawesome/DynamicFontAwesomeIcon.tsx
+++ src/components/fontawesome/DynamicFontAwesomeIcon.tsx
@@ -1,11 +1,11 @@
-'use client'
-
-import dynamic from 'next/dynamic';
-import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
-
-const DynamicFontAwesomeIcon = dynamic<FontAwesomeIconProps>(
-    () => import('@fortawesome/react-fontawesome').then(mod => mod.FontAwesomeIcon),
-    { ssr: false }
-);
-
-export default DynamicFontAwesomeIcon;
+// 'use client'
+//
+// import dynamic from 'next/dynamic';
+// import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
+//
+// const DynamicFontAwesomeIcon = dynamic<FontAwesomeIconProps>(
+//     () => import('@fortawesome/react-fontawesome').then(mod => mod.FontAwesomeIcon),
+//     { ssr: false }
+// );
+//
+// export default DynamicFontAwesomeIcon;
