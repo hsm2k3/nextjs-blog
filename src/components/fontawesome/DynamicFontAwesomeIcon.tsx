7af1642ace3afb6ee124186fa4049a2ba2bb8a02
Alex B <mabrave1106@gmail.com>
2025-05-08 18:13:47 -0400

    fix: troubleshooting - added type

--- src/components/fontawesome/DynamicFontAwesomeIcon.tsx
+++ src/components/fontawesome/DynamicFontAwesomeIcon.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import dynamic from 'next/dynamic';
+import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
-const DynamicFontAwesomeIcon = dynamic(
+const DynamicFontAwesomeIcon = dynamic<FontAwesomeIconProps>(
     () => import('@fortawesome/react-fontawesome').then(mod => mod.FontAwesomeIcon),
     { ssr: false }
 );
