d9b3fa29f97e16e346cf591ec36c09749416250f
Alex B <mabrave1106@gmail.com>
2025-05-09 00:44:14 -0400

    fix: client side rendering only for both card components.

--- src/components/cards/CardDescription.tsx
+++ src/components/cards/CardDescription.tsx
@@ -1,11 +1,24 @@
 'use client'
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import cardStyles from "@/app/styles/css/card.module.css";
 import Link from "next/link";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const CardDescription = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    // Return a placeholder during server rendering
+    if (!isClient) {
+        return <div className={cardStyles.icons}>Loading social links...</div>;
+    }
+
+    // Full component renders only on the client
     return (
         <div>
             <h1>Alex Braverman</h1>
