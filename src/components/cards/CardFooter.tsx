27830bbf63fadc465a894ee1961f0d45bbb161aa
Alex B <mabrave1106@gmail.com>
2025-05-08 20:27:35 -0400

    fix: trying DynamicFontAwesomeIcon

--- src/components/cards/CardFooter.tsx
+++ src/components/cards/CardFooter.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 import cardStyles from "@/app/styles/css/card.module.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import DynamicFontAwesomeIcon from "@/components/fontawesome/DynamicFontAwesomeIcon";
 import {faGithub, faLinkedin, faStackOverflow, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {faFile} from "@fortawesome/free-solid-svg-icons";
 
@@ -15,7 +15,7 @@ const CardFooter = () => {
                     <a href="https://www.linkedin.com/in/alex-braverman/"
                        target="_blank"
                        className="icon brands fa-linkedin">
-                    <FontAwesomeIcon
+                    <DynamicFontAwesomeIcon
                         icon={faLinkedin}
                         style={{color: "rgba(0, 114, 177, 1.0)", fontSize: iconFontSize}}
                         title={"Check out my LinkedIn"}
@@ -26,7 +26,7 @@ const CardFooter = () => {
                     <a href="https://github.com/hsm2k3"
                        target="_blank"
                        className="icon brands fa-github">
-                    <FontAwesomeIcon
+                    <DynamicFontAwesomeIcon
                         icon={faGithub}
                         style={{color: "rgba(23, 21, 21, 1.0)", fontSize: iconFontSize}}
                         title={"Check out my GitHub"}
@@ -37,7 +37,7 @@ const CardFooter = () => {
                     <a href="https://www.twitter.com/AlexBraverman4"
                        target="_blank"
                        className="icon brands fa-twitter">
-                    <FontAwesomeIcon
+                    <DynamicFontAwesomeIcon
                         icon={faTwitter}
                         style={{color: "rgba(0, 172, 238, 1.0)", fontSize: iconFontSize}}
                         title={"Check out my Twitter"}
@@ -48,7 +48,7 @@ const CardFooter = () => {
                     <a href="https://stackoverflow.com/users/7092930/alex"
                        target="_blank"
                        className="icon brands fa-stack-overflow">
-                    <FontAwesomeIcon
+                    <DynamicFontAwesomeIcon
                         icon={faStackOverflow}
                         style={{color: "rgba(239, 130, 54, 1.0)", fontSize: iconFontSize}}
                         title={"Check out my Stack Overflow"}
@@ -56,7 +56,7 @@ const CardFooter = () => {
                     </a>
                 </li>
                 <li><a href="/assets/Alex%20Braverman%20Resume%202023.pdf" download>
-                    <FontAwesomeIcon
+                    <DynamicFontAwesomeIcon
                         icon={faFile}
                         style={{color: "rgba(23, 21, 21, 1.0)", fontSize: iconFontSize}}
                         title={"Download my resume"}
